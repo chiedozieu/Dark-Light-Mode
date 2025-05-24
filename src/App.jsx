c495deed64e7609c06b86cfcc353d0646ce633f6
chiedozieu <chiedozieugwor@gmail.com>
2025-05-23 20:33:15 -0400

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./components/Card";
+import ThemeBtn from "./components/ThemeBtn";
+import { ThemeProvider } from "./contexts/theme";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themMode, setThemMode] = useState("light");
 
+  const lightTheme = () => {
+    setThemMode("light");
+  };
+  const darkTheme = () => {
+    setThemMode("dark");
+  };
+
+  useEffect(() => {
+   document.querySelector('html').classList.remove("dark", "light")
+   document.querySelector('html').classList.add(themMode)
+  }, [themMode])
+  
   return (
-    <>
-      <h1 className='Text-2xl text-blue-800'>Hello World</h1>
-    </>
-  )
+    <ThemeProvider value={{ themMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+          <div className="flex justify-end m-5">
+            <ThemeBtn />
+          </div>
+          <Card />
+          <div className="w-full max-w-sm mx-auto"></div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
